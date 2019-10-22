74a6d5c85ea2eb6dab004193944a4042b813e083
gwena <gwena.cs@gmail.com>
2019-10-22 17:30:57 +0900

    Add utils

---
+++ utils.py
@@ -0,0 +1,13 @@
+import os
+
+__author__ = "Gwena Cunha"
+
+
+def get_project_path():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    return current_dir
+
+
+def ensure_dir(dir_name):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
