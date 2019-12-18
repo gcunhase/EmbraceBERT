b354180c4e868f6cb7a10d0611ac765b35a26f15
gwena <gwena.cs@gmail.com>
2019-12-18 17:04:06 +0900

    Update script to obtain mean and std from results

--- get_results_mean_std.py
+++ get_results_mean_std.py
@@ -2,10 +2,22 @@ import json
 import numpy as np
 
 
-# Complete
-# AskUbuntu
-root_dir = 'results/embracebert/askubuntu/askubuntu_complete_ep100_bs4_'
-# root_dir = '/media/ceslea/RESEARCH/stacked_debert/IntentClassifier-RoBERTa/results/results_complete_earlyStopWithEvalLoss/bert/askubuntu_ep3/askubuntu_bert_ep3_bs4_'
+# STTError
+#root_name = '/media/ceslea/DATA/EmbraceBERT-results-backup/'
+root_name = './'
+#stt_error, dataname, model, epoch, bs, tts_stt_type = [True, "webapplications", "roberta", 100, 4, 'gtts_sphinx']
+stt_error, dataname, model, epoch, bs, tts_stt_type = [True, "webapplications", "embracebert_condensed_withDropout0.3", 30, 4, 'gtts_sphinx']
+#stt_error, dataname, model, epoch, bs, tts_stt_type = [False, "webapplications", "embraceroberta_condensed_withDropout0.1", 100, 16, 'gtts_google']
+#stt_error, dataname, model, epoch, bs, tts_stt_type = [True, "snips", "roberta", 3, 16, 'macsay_witai']
+#stt_error, dataname, model, epoch, bs, tts_stt_type = [True, "askubuntu", "roberta", 100, 4, 'macsay_witai']
+#stt_error, dataname, model, epoch, bs, tts_stt_type = [False, "webapplications", "embracebert_withDropout0.3", 100, 16, 'gtts_google']
+if stt_error:
+    root_dir = '{root_name}results/{model}/{dataname}/stterror/{tts_stt_type}/{dataname}_ep{epoch}_bs{bs}_'.\
+        format(root_name=root_name, model=model, dataname=dataname, epoch=epoch, bs=bs, tts_stt_type=tts_stt_type)
+else:
+    root_dir = '{root_name}results/{model}/{dataname}/complete/{dataname}_ep{epoch}_bs{bs}_'.\
+        format(root_name=root_name, model=model, dataname=dataname, epoch=epoch, bs=bs)
+
 
 f1_micro_str_all = ""
 for perc in [0.1]:
