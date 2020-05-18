a3a99aeeaddf33bbd94f35c1ed1f9ef7433c7d75
gwena <gwena.cs@gmail.com>
2020-05-18 20:00:31 +0900

    Fix unsqueeze dimensions in AttentionLayer

--- models/AttentionLayer.py
+++ models/AttentionLayer.py
@@ -16,7 +16,10 @@ class AttentionLayer(nn.Module):
     def forward(self, cls_output, embraced_features_token, unsqueeze_idx=1):
         # Last stage: Apply attention layer to CLS and embraced_features_token
         query = torch.unsqueeze(cls_output, unsqueeze_idx).cuda()  # query = torch.randn(5, 1, 256)
-        context = torch.unsqueeze(embraced_features_token, unsqueeze_idx).cuda()  # context = torch.randn(5, 5, 256)
+        if len(embraced_features_token.shape) != 3:
+            context = torch.unsqueeze(embraced_features_token, unsqueeze_idx).cuda()  # context = torch.randn(5, 5, 256)
+        else:
+            context = embraced_features_token.cuda()
         embrace_output, weights = self.embrace_attention(query, context)
         embrace_output = embrace_output.squeeze()
 
